9a69bbea505d4bf03246655a11c69b3c8121e41c
Joss Moffatt <joss@Mac.mynet>
2026-03-20 22:55:02 +0000

    style: apply spotless

--- src/main/java/dev/joss/dagger/cucumber/DaggerBackend.java
+++ src/main/java/dev/joss/dagger/cucumber/DaggerBackend.java
@@ -3,7 +3,6 @@ package dev.joss.dagger.cucumber;
 import io.cucumber.core.backend.Backend;
 import io.cucumber.core.backend.Glue;
 import io.cucumber.core.backend.Snippet;
-
 import java.net.URI;
 import java.util.List;
 
@@ -11,23 +10,17 @@ import java.util.List;
 @SuppressWarnings("StrictUnusedVariable")
 public class DaggerBackend implements Backend {
 
-    @Override
-    public void loadGlue(Glue glue, List<URI> list) {
+  @Override
+  public void loadGlue(Glue glue, List<URI> list) {}
 
-    }
+  @Override
+  public void buildWorld() {}
 
-    @Override
-    public void buildWorld() {
+  @Override
+  public void disposeWorld() {}
 
-    }
-
-    @Override
-    public void disposeWorld() {
-
-    }
-
-    @Override
-    public Snippet getSnippet() {
-        return null;
-    }
+  @Override
+  public Snippet getSnippet() {
+    return null;
+  }
 }
--- src/test/java/dev/joss/dagger/cucumber/DaggerBackendTest.java
+++ src/test/java/dev/joss/dagger/cucumber/DaggerBackendTest.java
@@ -1,20 +1,19 @@
 package dev.joss.dagger.cucumber;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @ExtendWith(MockitoExtension.class)
 class DaggerBackendTest {
 
-    @InjectMocks DaggerBackend daggerBackend;
+  @InjectMocks DaggerBackend daggerBackend;
 
-    @Test
-    void correctlyInstantiatesDaggerBackend() {
-        assertThat(daggerBackend).isNotNull();
-    }
+  @Test
+  void correctlyInstantiatesDaggerBackend() {
+    assertThat(daggerBackend).isNotNull();
+  }
 }
